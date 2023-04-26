ece325bba73b983a6b9039792af6e40083ea15da
Sarina <hemmatpour.sarina@gmail.com>
2023-04-26 13:09:46 +0430

    rxjava operators

--- RxJavaFirst/app/src/main/java/com/example/rxjavafirst/MainActivity.java
+++ RxJavaFirst/app/src/main/java/com/example/rxjavafirst/MainActivity.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.MaybeObserver;
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Predicate;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,5 +46,78 @@ public class MainActivity extends AppCompatActivity {
                         Log.i(TAG, "onComplete: ");
                     }
                 });
+
+
+        //If we list all the natural numbers below 10 that are multiples of 3 or 5,
+        // we get 3, 5, 6 and 9. The sum of these multiples is 23.
+        // Find the sum of all the multiples of 3 or 5 below 1000.
+        Observable.range(1,999)
+                .filter(integer -> integer%3==0 || integer%5==0)
+                .reduce((x,y)-> x+y)
+                .subscribe(new MaybeObserver<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
+
+                    @Override
+                    public void onSuccess(Integer integer) {
+                        Log.i(TAG, "onSuccess: "+ integer); //onSuccess: 233168
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.i(TAG, "onComplete: ");
+                    }
+                });
+
+        //How could you create an Observable that emits the Fibonacci sequence?
+
+        fibObservable().subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.i(TAG, "["+integer+"] , ");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG, "onError: ");
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+
+
+    }
+
+    public Observable<Integer> fibObservable(){
+        return Observable.create((emitter -> {
+            int f0=0 , f1=1, fNext=0;
+            try {
+                while (!emitter.isDisposed()){
+                    emitter.onNext(fNext);
+                    fNext=f0+f1;
+                    f0=f1;
+                    f1=fNext;
+                }
+            }catch (Exception e){
+                if (!emitter.isDisposed()){
+                    emitter.onError(e);
+                }
+            }
+        }));
     }
 }
