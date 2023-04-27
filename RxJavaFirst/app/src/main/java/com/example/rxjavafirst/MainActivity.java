ee37e8ea5818f954ec96e4552be35cfd3136c1bc
Sarina <hemmatpour.sarina@gmail.com>
2023-04-27 11:31:50 +0430

    rxjava operators -- more

--- RxJavaFirst/app/src/main/java/com/example/rxjavafirst/MainActivity.java
+++ RxJavaFirst/app/src/main/java/com/example/rxjavafirst/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
         Observable.just("Tiger" , "bird","lion","Bear")
                 .subscribe(new Observer<String>() {
                     @Override
@@ -99,7 +100,195 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+         */
 
+
+        /*
+        //map
+        Observable.just(1 , 2, 3,4 , 5 ,6 ,7)
+                .map(n->"Number= "+n+", ")
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        Log.i(TAG, s);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+         */
+
+
+
+        /*
+        //flatmap
+        //tartibe zazmani hefz nemishe
+        //ye observable e jadid ijad mikone
+        Observable.just(1 , 2, 3,4 , 5 ,6 ,7)
+                .flatMap(n-> Observable.just(n , n*10 , n*100).delay(2,TimeUnit.SECONDS))
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+
+                        Log.i(TAG, "onNext: "+integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                        Log.i(TAG, "onComplete: ");
+                    }
+                });
+
+         */
+
+        /*
+        //concatMap
+        //mesle flatMap vali ba tartibe zamani
+        Observable.just(1 , 2, 3,4 , 5 ,6 ,7)
+                .concatMap(n-> Observable.just(n , n*10 , n*100).delay(2,TimeUnit.SECONDS))
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.i(TAG, "onNext: "+integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+         */
+
+
+        /*
+        //switchMap
+        //akharin observable ro negah midare
+        Observable.just(1 , 2, 3,4 , 5 ,6 ,7)
+                .switchMap(n-> Observable.just(n , n*10 , n*100).delay(2,TimeUnit.SECONDS))
+                .subscribe(new Observer<Integer>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.i(TAG, "onNext: "+integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+
+         */
+
+
+
+        /*
+        //interval
+        //mitune jaygozine timer e android beshe
+        // moshkelat: ro ye thread e digast - memory leak
+        Observable.interval(1 , 1 , TimeUnit.SECONDS)
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        disposable=d;
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+
+                        Log.i(TAG, "onNext: " + aLong.toString());
+
+                        //canceling timer
+                        if (aLong.toString().equals("10"))
+                            disposable.dispose();
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+         */
+
+        //timer
+        //bade ye meghdar zaman yek item ra emit mikonad
+        Observable.timer(1 , TimeUnit.SECONDS)
+                .doFinally(()->{
+                    Log.i(TAG, "The End.");
+                })
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        Log.i(TAG, "onNext: " + aLong.toString());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
 
     }
 
