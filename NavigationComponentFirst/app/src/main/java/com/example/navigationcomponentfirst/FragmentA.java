e26c8a47fd23f10f62b6e054038426f74ad7654b
Sarina <hemmatpour.sarina@gmail.com>
2023-04-28 18:38:06 +0430

    first navigation component

---
+++ NavigationComponentFirst/app/src/main/java/com/example/navigationcomponentfirst/FragmentA.java
@@ -0,0 +1,32 @@
+package com.example.navigationcomponentfirst;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class FragmentA extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_a, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnGo=view.findViewById(R.id.button);
+        btnGo.setOnClickListener(v-> {
+            Navigation.findNavController(v).navigate(R.id.action_fragmentA_to_fragmentB);
+        });
+    }
+}
